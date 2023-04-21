3d4f6d393a233dbc83f17c63b9e683d1d6bc9d1b
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-04-21 14:12:14 +0100

    Dev

--- Admin/settings.py
+++ Admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-da-x$kst31c-lp=3uvt6#(^udl-k(+56xy7wz)ms&j7emrqj_p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
